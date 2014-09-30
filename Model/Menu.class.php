346fb1383fbbea9663575ab532131399e842ba72
LuoBoss <dev@pescms.com>
2014-09-30 11:31:26 +0800

    修复子列菜单删除引起父级菜单不显示的问题

--- Model/Menu.class.php
+++ Model/Menu.class.php
@@ -20,21 +20,24 @@ class Menu extends Model {
      * 生成后台菜单
      */
     public static function menu() {
-        $result = self::db('menu m')->join("(SELECT `menu_id` AS top_id, `menu_name` AS top_name, `menu_pid` AS top_pid, `menu_listsort` AS top_listsort FROM `pes_menu` where menu_pid = 0) AS parent ON parent.top_id = m.menu_pid")->where('top_pid = 0')->order('parent.top_listsort desc, m.menu_listsort desc, top_id asc, menu_id asc')->select();
+        $result = self::db('menu AS m')->field("m.*, IF(parent.top_id IS NULL, m.menu_id, parent.top_id) AS top_id, IF(parent.top_listsort IS NULL, '0', parent.top_listsort) AS top_listsort, IF(parent.top_name IS NULL, m.menu_name, top_name) AS top_name")->join("(SELECT `menu_id` AS top_id, `menu_name` AS top_name, `menu_pid` AS top_pid, `menu_listsort` AS top_listsort FROM `pes_menu` where menu_pid = 0) AS parent ON parent.top_id = m.menu_pid")->order('top_listsort desc, m.menu_listsort desc, m.menu_id asc')->select();
         foreach ($result as $key => $value) {
-            $menu[$value['top_name']]['menu_id'] = $value['top_id'];
-            $menu[$value['top_name']]['menu_name'] = $value['top_name'];
-            $menu[$value['top_name']]['menu_listsort'] = $value['top_listsort'];
-            $menu[$value['top_name']]['menu_child'][] = $value;
+            if ($value['menu_pid'] == 0) {
+                $menu[$value['top_name']]['menu_id'] = $value['top_id'];
+                $menu[$value['top_name']]['menu_name'] = $value['top_name'];
+                $menu[$value['top_name']]['menu_listsort'] = $value['menu_listsort'];
+            } else {
+                $menu[$value['top_name']]['menu_child'][] = $value;
+            }
         }
         return $menu;
     }
-    
+
     /**
      * 根据菜单获取标题
      */
-    public static function getTitleWithMenu(){
-        $result = self::db('menu')->field('menu_name')->where('menu_url = :menu_url')->find(array('menu_url' => 'Admin-'.MODULE."-".ACTION));
+    public static function getTitleWithMenu() {
+        $result = self::db('menu')->field('menu_name')->where('menu_url = :menu_url')->find(array('menu_url' => 'Admin-' . MODULE . "-" . ACTION));
         return $result['menu_name'];
     }
 
