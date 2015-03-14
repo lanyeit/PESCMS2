af7b1a89243907e8f307860c367755350249d0c0
LuoBoss <dev@pescms.com>
2015-03-14 17:40:15 +0800

    修复SSL下，多说插件无法加载的问题

--- Theme/Home/DOC/header.php
+++ Theme/Home/DOC/header.php
@@ -5,14 +5,14 @@
         <meta name="generator" content="ApiGen 2.8.0" />
 
         <title><?= empty($title) ? "" : "{$title} - "; ?><?= empty($cat['cate_name']) ? "" : "{$cat['cate_name']} - "; ?><?= $sitetitle; ?></title>
-        <link type="text/css" rel="stylesheet" href="<?= $siteurl; ?>/Theme/Origin/syntax/styles/shCore.css"/>
-        <link type="text/css" rel="stylesheet" href="<?= $siteurl; ?>/Theme/Origin/syntax/styles/shThemeDefault.css"/>
-        <link rel="stylesheet" type="text/css" media="all" href="<?= $siteurl; ?>/Theme/Home/DOC/css/style.css" />
-        <script type="text/javascript" src="<?= $siteurl; ?>/Theme/Home/DOC/js/combined.js"></script>
-        <script type="text/javascript" src="<?= $siteurl; ?>/Theme/Home/DOC/js/elementlist.js"></script>
-        <script type="text/javascript" src="<?= $siteurl; ?>/Theme/Origin/syntax/scripts/brush.js"></script>
+        <link type="text/css" rel="stylesheet" href="/Theme/Origin/syntax/styles/shCore.css"/>
+        <link type="text/css" rel="stylesheet" href="/Theme/Origin/syntax/styles/shThemeDefault.css"/>
+        <link rel="stylesheet" type="text/css" media="all" href="/Theme/Home/DOC/css/style.css" />
+        <script type="text/javascript" src="/Theme/Home/DOC/js/combined.js"></script>
+        <script type="text/javascript" src="/Theme/Home/DOC/js/elementlist.js"></script>
+        <script type="text/javascript" src="/Theme/Origin/syntax/scripts/brush.js"></script>
         <script class="javascript">
-            SyntaxHighlighter.config.clipboardSwf = '<?= $siteurl; ?>/Theme/Origin/syntax/scripts/clipboard.swf';
+            SyntaxHighlighter.config.clipboardSwf = '/Theme/Origin/syntax/scripts/clipboard.swf';
             SyntaxHighlighter.all();
         </script>
     </head>
